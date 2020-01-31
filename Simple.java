12a44c879ce7552c62ff2346cdfebca9c7deddeb
Hasib Rostaiee <60456566+hrostaiee@users.noreply.github.com>
2020-01-30 22:33:41 -0500

    Adding Simple source java and class file

---
+++ Simple.java
@@ -0,0 +1,10 @@
+//This is first and simple java class
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Programming is fun!");
+	}
+
+}
